d263ed1766c71e9139400f0d6fb0d7792fb8e807
johnyu <johnyu@kingwaytek.com>
2015-07-18 10:18:39 +0800

    加入controller,使用Redirect重導至靜態網頁

---
+++ SimpleDemo/Controllers/DataTableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleDemo.Controllers
+{
+    public class DataTableController : Controller
+    {
+        // GET: DataTable
+        public ActionResult Test0()
+        {
+            return Redirect("~/DataTableTest0.html");
+        }
+    }
+}
